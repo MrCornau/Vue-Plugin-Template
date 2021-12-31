336ce513f8ceb63218b2d78e7ce473327e82a7af
Josh Cornau <hi@joshcornau.de>
2021-12-31 11:57:59 +0100

    Switched ui to vue.js

--- src/ui.vue
+++ src/ui.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
-div
-	button.button.button--primary(@click='createNode') Create a node
-	p.type.type--pos-small-normal {{message}}
+//- div
+//- 	button.button.button--primary(@click='createNode') Create a TEST node
+//- 	p.type.type--pos-small-normal {{message}}
+//- div
+
+<div>
+<input v-model="message" placeholder="edit me">
+<p>Message is: {{ message }}</p>
+<button class='button button--primary' @click='createNode(message)'>Label</button> 
+</div>
 </template>
 
 <script>
 import { dispatch, handleEvent } from "./uiMessageHandler";
-
 // Add these lines to import the interactive figma-ui components as needed.
 import "./figma-ui/js/selectMenu";
 import "./figma-ui/js/iconInput";
@@ -15,7 +21,7 @@ import "./figma-ui/js/disclosure";
 export default {
   data() {
     return {
-      message: ""
+      message: "",
     };
   },
   mounted() {
@@ -25,19 +31,20 @@ export default {
     window.disclosure.init();
 
     // The following shows how messages from the main code can be handled in the UI code.
-    handleEvent("nodeCreated", nodeID => {
+    handleEvent("nodeCreated", (nodeID) => {
       this.message = `Node ${nodeID} was created!`;
     });
   },
   methods: {
-    createNode() {
-      // This shows how the UI code can send messages to the main code.
-      dispatch("createNode");
-    }
-  }
+    createNode(message) {
+      console.log(message, "test");
+      let numbers = message.split(",").map((x) => +x);
+      dispatch("createNode", numbers);
+    },
+  },
 };
 </script>
 
-<style lang='scss'>
+<style lang="scss">
 @import "./figma-ui/figma-plugin-ds";
 </style>
