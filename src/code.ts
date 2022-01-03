d1228993ca70f2cd4d9829e3b091063fe6738374
Josh Cornau <hi@joshcornau.de>
2022-01-03 09:27:32 +0100

    first itteration -

--- src/code.ts
+++ src/code.ts
@@ -1,78 +1,227 @@
 import { dispatch, handleEvent } from './codeMessageHandler';
+const arrow = require("./Arrow.svg")
+
 figma.showUI(__html__);
+
+let backgroundcolor = {r:35,b:37,g:38}
+let fontcolor_primary = { r: 0.82745, g: 0.84314, b: 0.84706 }
+let color_accent = { r: 0.86667, g: 0.89804, b: 0.55686 }
+let color_background = { r: 0.13725, g: 0.14510, b: 0.14902 }
+let transparent = {visible:false, type : "SOLID", color: fontcolor_primary}
+
 
 
 figma.ui.onmessage = async (message) => {
 	// Roboto Regular is the font that objects will be created with by default in
 	// Figma. We need to wait for fonts to load before creating text using them.
-	await figma.loadFontAsync({ family: "Roboto", style: "Regular" })
-	let numbers = message.data
 	
-	const frameWidth = 800
+
+	console.log(message)
+
+	let data = message.data
+	let autor = message.data[0].autor
+	let date = message.data[0].date
+	let sent = message.data[0].MarkedSent
+	let link = message.data[0].link
+
+	console.log(autor,'Autor')
+	await figma.loadFontAsync({ family: "IBM Plex Serif", style: "Regular" })
+
+    let boldFont = {family: "IBM Plex Serif", style: "Bold"};
+    await figma.loadFontAsync(boldFont);
+	let italicFont = {family: "IBM Plex Sans", style: "Italic"};
+	await figma.loadFontAsync(italicFont);
+	// let numbers = message.data
+	
+	const frameWidth = 480
 	const frameHeight = 600
-	const chartX = 25
-	const chartY = 50
-	const chartWidth = frameWidth - 50
-	const chartHeight = frameHeight - 50
+    const imageWidth = 320;
+    const cardPadding = 16;
+    // Create Metacard Frame
+    let cardFrame = figma.createFrame();
+	// @ts-ignore
+    cardFrame.cornerRadius = 0;
+	cardFrame.resize(imageWidth + cardPadding*2,imageWidth);
+    cardFrame.layoutMode = "VERTICAL";
+	// @ts-ignore
+    cardFrame.paddingLeft = cardFrame.paddingRight = cardFrame.paddingTop = cardFrame.paddingBottom = cardPadding;
+    cardFrame.itemSpacing = cardPadding/2;
+	// @ts-ignore
+    cardFrame.primaryAxisSizingMode = "AUTO";
+    cardFrame.counterAxisSizingMode = "FIXED";
+    cardFrame.fills = [{type : "SOLID", color: color_background}];
+    // cardFrame.strokes = [{type : "SOLID", color: { r: 0, g: 0, b: 0 }}];
+    cardFrame.strokeWeight = 2;
+	
+	// const newNode = figma.createNodeFromSvg(arrow);
+	// header container
+
+	let header = figma.createFrame();
+	cardFrame.appendChild(header);
+	header.layoutMode = "HORIZONTAL";
+	// @ts-ignore
+	header.primaryAxisSizingMode = "FIXED";
+	header.counterAxisSizingMode = "AUTO";
+	// @ts-ignore
+	header.primaryAxisAlignItems = "SPACE_BETWEEN";
+	header.layoutAlign= "STRETCH";
+	// @ts-ignore
+	header.fills = [transparent]
+
+	//heading
+	let heading = figma.createFrame();
+	header.appendChild(heading);
+	heading.layoutMode = "VERTICAL";
+	heading.counterAxisSizingMode = "AUTO";
+	// @ts-ignore
+	heading.fills = [transparent]
+	
+	//title
+	let Title = figma.createText();
+	heading.appendChild(Title);
+    Title.fontName = boldFont;
+	Title.fills = [{type : "SOLID", color: fontcolor_primary}]
+    Title.characters = autor;
+    Title.textAutoResize = "HEIGHT";
+    Title.layoutAlign = "STRETCH";
+	
+	//date
+	let date_T = figma.createText();
+	heading.appendChild(date_T);
+    date_T.fontName = italicFont;
+	date_T.fills = [{type : "SOLID", color: color_accent}]
+    date_T.characters = date;
+    date_T.textAutoResize = "WIDTH_AND_HEIGHT";
+    date_T.layoutAlign = "STRETCH";
+
+	// Tag
+	let tag = figma.createFrame();
+	header.appendChild(tag);
+	tag.layoutMode = "VERTICAL";
+	tag.itemSpacing = 1;
+	tag.counterAxisSizingMode = "AUTO";
+	// @ts-ignore
+	tag.primaryAxisSizingMode = "AUTO";
+	tag.fills = [{opacity:0.25, type : "SOLID", color: color_accent}]
+
+	let tag_T = figma.createText();
+	tag.appendChild(tag_T);
+    tag_T.fontName = italicFont;
+	tag_T.fills = [{type : "SOLID", color: color_accent}]
+    tag_T.characters = '#comunication';
+    tag_T.textAutoResize = "WIDTH_AND_HEIGHT";
+    tag_T.layoutAlign = "STRETCH";
+	
+
+	// description
+	let description_T = figma.createText();
+	cardFrame.appendChild(description_T);
+	description_T.fontName = italicFont;
+	description_T.fills = [{type : "SOLID", color: fontcolor_primary}]
+    description_T.characters = sent;
+    description_T.textAutoResize = "WIDTH_AND_HEIGHT";
+    description_T.layoutAlign = "STRETCH";
+
+	// link
+	// Tag
+	let link_F = figma.createFrame();
+	cardFrame.appendChild(link_F);
+	link_F.layoutMode = "VERTICAL";
+	link_F.itemSpacing = 1;
+	link_F.counterAxisSizingMode = "AUTO";
+	// @ts-ignore
+	link_F.primaryAxisSizingMode = "AUTO";
+	// @ts-ignore
+	link_F.fills =  [transparent]
+
+	let Link_T = figma.createText();
+	link_F.appendChild(Link_T);
+    Link_T.fontName = italicFont;
+	Link_T.fills = [{type : "SOLID", color: fontcolor_primary}]
+    Link_T.characters = 'visit comment';
+    Link_T.textAutoResize = "WIDTH_AND_HEIGHT";
+    Link_T.layoutAlign = "STRETCH";
+	// @ts-ignore
+	Link_T.hyperlink = {
+		type: "URL",
+		value: link
+	  };
+
+
+	// let metaDesc = figma.createText();
+    // await figma.loadFontAsync({family: "Inter", style: "Medium"});
+    // cardFrame.appendChild(metaDesc);
+    // metaDesc.characters = sent;
+    // metaDesc.textAutoResize = "HEIGHT";
+    // metaDesc.layoutAlign = "STRETCH";
+	
+    // metaTitle.hyperlink = {
+    //   type: "URL",
+    //   value: metaTags.hyperlink
+    // };
+
+
+	// const chartX = 25
+	// const chartY = 50
+	// const chartWidth = frameWidth - 50
+	// const chartHeight = frameHeight - 50
+	// frame.resizeWithoutConstraints(frameWidth, frameHeight) //Size of frame
   
-	const frame = figma.createFrame()
-	frame.resizeWithoutConstraints(frameWidth, frameHeight) //Size of frame
+	// // Center the frame in our current viewport so we can see it.
+	// frame.x = figma.viewport.center.x - frameWidth / 2
+	// frame.y = figma.viewport.center.y - frameHeight / 2
   
-	// Center the frame in our current viewport so we can see it.
-	frame.x = figma.viewport.center.x - frameWidth / 2
-	frame.y = figma.viewport.center.y - frameHeight / 2
+	// // Border for the chart
+	// const border = figma.createRectangle()
+	// frame.appendChild(border)
+	// border.resizeWithoutConstraints(frameWidth, frameHeight)
+	// border.strokeAlign = 'INSIDE'
+	// border.strokeWeight = 3
+	// border.fills = []
+	// border.strokes = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
+	// border.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
   
-	// Border for the chart
-	const border = figma.createRectangle()
-	frame.appendChild(border)
-	border.resizeWithoutConstraints(frameWidth, frameHeight)
-	border.strokeAlign = 'INSIDE'
-	border.strokeWeight = 3
-	border.fills = []
-	border.strokes = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
-	border.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
+	// // Line at the bottom of the chartx^x
+	// const line = figma.createRectangle()
+	// frame.appendChild(line)
+	// line.x = chartX
+	// line.y = chartY + chartHeight
+	// line.resizeWithoutConstraints(chartWidth, 3)
+	// line.fills = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
+	// line.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
   
-	// Line at the bottom of the chart
-	const line = figma.createRectangle()
-	frame.appendChild(line)
-	line.x = chartX
-	line.y = chartY + chartHeight
-	line.resizeWithoutConstraints(chartWidth, 3)
-	line.fills = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
-	line.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
+	// const min = numbers.reduce((a, b) => Math.min(a, b), 0)
+	// const max = numbers.reduce((a, b) => Math.max(a, b), 0)
   
-	const min = numbers.reduce((a, b) => Math.min(a, b), 0)
-	const max = numbers.reduce((a, b) => Math.max(a, b), 0)
+	// for (let i = 0; i < numbers.length; i++) {
+	//   const num = numbers[i];
+	//   const left = chartX + chartWidth * (i + 0.25) / numbers.length;
+	//   const right = chartX + chartWidth * (i + 0.75) / numbers.length;
+	//   const top = chartY + chartHeight - chartHeight * (Math.max(0, num) - min) / (max - min);
+	//   const bottom = chartY + chartHeight - chartHeight * (Math.min(0, num) - min) / (max - min);
   
-	for (let i = 0; i < numbers.length; i++) {
-	  const num = numbers[i];
-	  const left = chartX + chartWidth * (i + 0.25) / numbers.length;
-	  const right = chartX + chartWidth * (i + 0.75) / numbers.length;
-	  const top = chartY + chartHeight - chartHeight * (Math.max(0, num) - min) / (max - min);
-	  const bottom = chartY + chartHeight - chartHeight * (Math.min(0, num) - min) / (max - min);
+	//   // The column
+	//   const column = figma.createRectangle()
+	//   frame.appendChild(column)
+	//   column.x = left
+	//   column.y = top
+	//   column.resizeWithoutConstraints(right - left, bottom - top)
+	//   column.fills = [{ type: 'SOLID', color: {r: 1, g: 0, b: 0} }]
+	//   column.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
   
-	  // The column
-	  const column = figma.createRectangle()
-	  frame.appendChild(column)
-	  column.x = left
-	  column.y = top
-	  column.resizeWithoutConstraints(right - left, bottom - top)
-	  column.fills = [{ type: 'SOLID', color: {r: 1, g: 0, b: 0} }]
-	  column.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-  
-	  // The label
-	  const label = figma.createText()
-	  frame.appendChild(label)
-	  label.x = left - 50
-	  label.y = top - 50
-	  label.resizeWithoutConstraints(right - left + 100, 50)
-	  label.fills = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
-	  label.characters = num.toString()
-	  label.fontSize = 30
-	  label.textAlignHorizontal = 'CENTER'
-	  label.textAlignVertical = 'BOTTOM'
-	  label.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-	}
+	//   // The label
+	//   const label = figma.createText()
+	//   frame.appendChild(label)
+	//   label.x = left - 50
+	//   label.y = top - 50
+	//   label.resizeWithoutConstraints(right - left + 100, 50)
+	//   label.fills = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
+	//   label.characters = num.toString()
+	//   label.fontSize = 30
+	//   label.textAlignHorizontal = 'CENTER'
+	//   label.textAlignVertical = 'BOTTOM'
+	//   label.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
+	// }
   
 	figma.closePlugin()
   }
--- src/ui.vue
+++ src/ui.vue
@@ -5,8 +5,8 @@
 //- div
 
 <div>
-<input v-model="message" placeholder="edit me">
-<p>Message is: {{ message }}</p>
+//- <input v-model="message" placeholder="edit me">
+//- <p>Message is: {{ message }}</p>
 <button class='button button--primary' @click='createNode(message)'>Label</button> 
 </div>
 </template>
@@ -21,7 +21,27 @@ import "./figma-ui/js/disclosure";
 export default {
   data() {
     return {
-      message: "",
+      message: [
+        {
+          autor: "ntoporcov",
+          date: "2019-01-18 06:21:37",
+          content:
+            "I created a web app for photographers who use Google Drive to send photos to clients but wish they had a better looking page to send to clients.",
+          link: "https://www.reddit.com/r/Nikon/comments/ah7lvu/i_created_a_web_app_for_photographers_who_use/",
+          origin: "Reddit",
+          suborigin: "nikon",
+          result: true,
+          Selector: "i created",
+          selectorShort: "create",
+          MarkedSent:
+            "-----> i !!!  -----> created !!!  a web app for photographers who use google drive to send photos to clients but wish they had a better looking page to send to clients.",
+          sortedWord: "None",
+          Identifyer: null,
+          identifyer: 1248,
+          year: "2019",
+          id: 0,
+        },
+      ],
     };
   },
   mounted() {
@@ -37,9 +57,9 @@ export default {
   },
   methods: {
     createNode(message) {
-      console.log(message, "test");
-      let numbers = message.split(",").map((x) => +x);
-      dispatch("createNode", numbers);
+      // console.log(message, "test");
+      // let numbers = message.split(",").map((x) => +x);
+      dispatch("createNode", message);
     },
   },
 };
