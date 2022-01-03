1f822e831338f36c8a75a28f0766cff931713089
Josh Cornau <hi@joshcornau.de>
2022-01-03 15:40:49 +0100

    Get Marked Parts of Sent

--- src/code.ts
+++ src/code.ts
@@ -19,6 +19,8 @@ figma.ui.onmessage = async (message) => {
 	// Figma. We need to wait for fonts to load before creating text using them.
 	///console.log(message)
 
+	
+
 	let data = message.data
 	let autor = message.data[0].autor
 	let date = message.data[0].date
@@ -27,18 +29,40 @@ figma.ui.onmessage = async (message) => {
 
 
 
-	// var needle = '---->'
-	// var re = new RegExp(needle,'gi');
-	// var haystack = sent;
+	let start = '-----> '
+	let end = ' !!! '
+	console.log(start.length)
+	let reStart = new RegExp(start,'gi');
+	let reEnd = new RegExp(end,'gi');
 	
-	// var results = new Array();//this is the results you want
-	// while (re.exec(haystack)){
-	//   results.push(re.lastIndex);
-	// }
+	let startMark = new Array();//this is the results you want
+	let endMark = new Array();
+	while (reStart.exec(sent)){
+		startMark.push(reStart.lastIndex);
+	}
+	while (reEnd.exec(sent)){
+		endMark.push(reEnd.lastIndex);
+	}
 
-	// console.log(results)
+
+	console.log(startMark,endMark)
 	//console.log(autor,'Autor')
-	await figma.loadFontAsync({ family: "IBM Plex Serif", style: "Regular" })
+	let newArray =  new Array();
+	startMark.map((currElement, index) => {
+		let subtract = ((index+1)*start.length)+(index*end.length)
+		newArray.push(currElement - subtract)
+	})
+	console.log(newArray)
+
+
+	let newArray2 =  new Array();
+	endMark.map((currElement, index) => {
+		let subtract = ((index+1)*start.length)+((index+1)*end.length)
+		newArray2.push(currElement - subtract)
+	})
+	console.log(newArray2)
+
+
 
     let boldFont = {family: "IBM Plex Serif", style: "Bold"};
     await figma.loadFontAsync(boldFont);
@@ -136,10 +160,7 @@ figma.ui.onmessage = async (message) => {
     description_T.textAutoResize = "WIDTH_AND_HEIGHT";
     description_T.layoutAlign = "STRETCH";
 
-	console.log(description_T.fills)
 	// link
-
-
 
 
 
@@ -169,82 +190,6 @@ figma.ui.onmessage = async (message) => {
 		type: "URL",
 		value: link
 	  };
-
-
-	// let metaDesc = figma.createText();
-    // await figma.loadFontAsync({family: "Inter", style: "Medium"});
-    // cardFrame.appendChild(metaDesc);
-    // metaDesc.characters = sent;
-    // metaDesc.textAutoResize = "HEIGHT";
-    // metaDesc.layoutAlign = "STRETCH";
-	
-    // metaTitle.hyperlink = {
-    //   type: "URL",
-    //   value: metaTags.hyperlink
-    // };
-
-
-	// const chartX = 25
-	// const chartY = 50
-	// const chartWidth = frameWidth - 50
-	// const chartHeight = frameHeight - 50
-	// frame.resizeWithoutConstraints(frameWidth, frameHeight) //Size of frame
-  
-	// // Center the frame in our current viewport so we can see it.
-	// frame.x = figma.viewport.center.x - frameWidth / 2
-	// frame.y = figma.viewport.center.y - frameHeight / 2
-  
-	// // Border for the chart
-	// const border = figma.createRectangle()
-	// frame.appendChild(border)
-	// border.resizeWithoutConstraints(frameWidth, frameHeight)
-	// border.strokeAlign = 'INSIDE'
-	// border.strokeWeight = 3
-	// border.fills = []
-	// border.strokes = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
-	// border.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-  
-	// // Line at the bottom of the chartx^x
-	// const line = figma.createRectangle()
-	// frame.appendChild(line)
-	// line.x = chartX
-	// line.y = chartY + chartHeight
-	// line.resizeWithoutConstraints(chartWidth, 3)
-	// line.fills = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
-	// line.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-  
-	// const min = numbers.reduce((a, b) => Math.min(a, b), 0)
-	// const max = numbers.reduce((a, b) => Math.max(a, b), 0)
-  
-	// for (let i = 0; i < numbers.length; i++) {
-	//   const num = numbers[i];
-	//   const left = chartX + chartWidth * (i + 0.25) / numbers.length;
-	//   const right = chartX + chartWidth * (i + 0.75) / numbers.length;
-	//   const top = chartY + chartHeight - chartHeight * (Math.max(0, num) - min) / (max - min);
-	//   const bottom = chartY + chartHeight - chartHeight * (Math.min(0, num) - min) / (max - min);
-  
-	//   // The column
-	//   const column = figma.createRectangle()
-	//   frame.appendChild(column)
-	//   column.x = left
-	//   column.y = top
-	//   column.resizeWithoutConstraints(right - left, bottom - top)
-	//   column.fills = [{ type: 'SOLID', color: {r: 1, g: 0, b: 0} }]
-	//   column.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-  
-	//   // The label
-	//   const label = figma.createText()
-	//   frame.appendChild(label)
-	//   label.x = left - 50
-	//   label.y = top - 50
-	//   label.resizeWithoutConstraints(right - left + 100, 50)
-	//   label.fills = [{ type: 'SOLID', color: {r: 0, g: 0, b: 0} }]
-	//   label.characters = num.toString()
-	//   label.fontSize = 30
-	//   label.textAlignHorizontal = 'CENTER'
-	//   label.textAlignVertical = 'BOTTOM'
-	//   label.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'}
-	// }
   
 	figma.closePlugin()
   }
